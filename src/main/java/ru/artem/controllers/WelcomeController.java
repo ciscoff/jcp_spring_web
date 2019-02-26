a8c9ec9d5974937ff47775cf52939adc1e3e962b
yagray <yagray@mail.ru>
2019-02-26 19:36:06 +0300

    Добавил страничку news.html и сервлет для неё. Работает ))

--- src/main/java/ru/artem/controllers/WelcomeController.java
+++ src/main/java/ru/artem/controllers/WelcomeController.java
@@ -9,18 +9,37 @@ import java.util.Map;
 public class WelcomeController {
     String param = "";
 
+    /**
+     * @RequestMapping
+     * Фактически это сервлет. В архитектуре MVC он является контроллером, который получает
+     * запросы из сети (от Apache), обрабатывает их и генерит результирующую html-страницу (View).
+     * Шаблон страницы у нас уже есть в папке templates. Нужно просто передать в неё данные,
+     * которые надо отобразить. Делается это через Model.
+     * Итого имеем:
+     *  Model        - результат обработки web-запроса, помещенный в Map<T,U>
+     *  View         - шаблом html-страницы
+     *  Controller   - сервлет
+     *
+     */
     @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
         model.put("message", "Java!");
-        return "welcome";
+        return "welcome";           // Имя возвращаемой страницы html из пакета templates
     }
-
 
     @RequestMapping("/about")
     public String about(Map<String, Object> model) {
         model.put("test_param", param);
         return "about";
     }
+
+    @RequestMapping("/news")
+    public String news(Map<String, Object> model) {
+        model.put("spring", "Spring is great");
+        model.put("techno", "technology");
+        return "news";
+    }
+
 
     @RequestMapping(value = "/printme/{data}", method = RequestMethod.GET)
     public String printme(Map<String, Object> model, @PathVariable("data") String data) {
